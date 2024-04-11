28ab761560d8bd7a00b806e9e13429037d2857bb
ElenaAncaIrimia <anca.irimia244@gmail.com>
2024-04-11 12:05:06 +0200

    ejercicio1

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function change() {
     // your code goes here
+    const titleByClassName = document.querySelector(".titleName");
+    const titleTextByClass = titleByClassName.textContent;
+    const titleTextById = document.querySelector("#titleId").textContent;
+    
+    
+    console.log("Title text by class name:", titleTextByClass);
+    console.log (titleTextById);
 }
