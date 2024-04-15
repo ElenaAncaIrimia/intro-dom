1d09f6b418c3e98fbecf6a740ca0cf34cac52131
ElenaAncaIrimia <anca.irimia244@gmail.com>
2024-04-15 10:23:11 +0200

    exOne

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 function change() {
-    // your code goes here
-    const titleByClassName = document.querySelector(".titleName");
-    const titleTextByClass = titleByClassName.textContent;
-    const titleTextById = document.querySelector("#titleId").textContent;
-    
-    
-    console.log("Title text by class name:", titleTextByClass);
-    console.log (titleTextById);
-}
+  // your code goes here
+  let titleByClassName = document.querySelector(".titleName");
+
+  titleByClassName.textContent = "Holi coders!";
+
+  if (titleByClassName.textContent === "Holi coders!") {
+    return titleByClassName.textContent === "Lorem, ipsum dolor";
+  } 
+    return titleByClassName.textContent = "Holi coders!";
+  }
+  // const titleTextById = document.querySelector("#titleId").textContent;
+  // //
+
+  // console.log("Title text by class name:", titleTextByClass);
+  // console.log (titleTextById);
+
