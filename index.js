9e7fe3f6db2893ab3ac3a392c3c89bef7139ec13
ElenaAncaIrimia <anca.irimia244@gmail.com>
2024-04-15 10:28:20 +0200

    exTwo

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ function change() {
     // your code goes here
     const newTitleColour = document.querySelector("titleClassName");
     newTitleColour.style.color = "red";
+    console.log(newTitleColour);
 }
